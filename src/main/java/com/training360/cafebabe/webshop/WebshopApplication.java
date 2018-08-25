0db871aef4e5359cd874a9782aaba2a07dee8234
Norbert Miko <m.norbert96@gmail.com>
2018-08-25 15:27:46 +0200

    initial commit

---
+++ src/main/java/com/training360/cafebabe/webshop/WebshopApplication.java
@@ -0,0 +1,12 @@
+package com.training360.cafebabe.webshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebshopApplication.class, args);
+	}
+}
